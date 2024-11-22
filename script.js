da4068c6baf646147841baac50b7db2445d139c7
LarsMasonsmith <71853565+LarsMasonsmith@users.noreply.github.com>
2024-11-22 15:40:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+function getNuclearCNum(){
+    let nuclearCNum = document.getElementById("nuclearNumber").value;
+    document.getElementById("nuclearNumber").value = '';
+    console.log(nuclearCNum);
+}
+function getNuclearDNum(){
+    let nuclearDNum = document.getElementById("nuclearNumber").value;
+    document.getElementById("nuclearNumber").value = '';
+    console.log(nuclearDNum);
+}
+function getCoalCNum(){
+    let coalCNum = document.getElementById("coalNumber").value;
+    document.getElementById("coalNumber").value = '';
+    console.log(coalCNum);
+}
+function getCoalDNum(){
+    let coalDNum = document.getElementById("coalNumber").value;
+    document.getElementById("coalNumber").value = '';
+    console.log(coalDNum);
+}
+function getCoalNNum(){
+    let coalNNum = document.getElementById("coalNumber").value;
+    document.getElementById("coalNumber").value = '';
+    console.log(coalNNum);
+}
+function getGasCNum(){
+    let gasCNum = document.getElementById("gasNumber").value;
+    document.getElementById("gasNumber").value = '';
+    console.log(gasCNum);
+}
+function getGasDNum(){
+    let gasDNum = document.getElementById("gasNumber").value;
+    document.getElementById("gasNumber").value = '';
+    console.log(gasDNum);
+}
+function getWindCNum(){
+    let windCNum = document.getElementById("windNumber").value;
+    document.getElementById("windNumber").value = '';
+    console.log(windCNum);
+}
+function getWindDNum(){
+    let windDNum = document.getElementById("windNumber").value;
+    document.getElementById("windNumber").value = '';
+    console.log(windDNum);
+}
+function getSolarCNum(){
+    let solarCNum = document.getElementById("solarNumber").value;
+    document.getElementById("solarNumber").value = '';
+    console.log(solarCNum);
+}
+function getSolarDNum(){
+    let solarDNum = document.getElementById("solarNumber").value;
+    document.getElementById("solarNumber").value = '';
+    console.log(solarDNum);
+}
+function getBatteryCNum(){
+    let batteryCNum = document.getElementById("batteryNumber").value;
+    document.getElementById("batteryNumber").value = '';
+    console.log(batteryCNum);
+}
+function getBatteryDNum(){
+    let batteryDNum = document.getElementById("batteryNumber").value;
+    document.getElementById("batteryNumber").value = '';
+    console.log(batteryDNum);
+}
